771670548cdcc48a681740ad818a98aa4b76f690
lenovo <2972591603@qq.com>
2020-04-10 14:03:16 +0800

    0410考试

---
+++ app/src/main/java/com/bawei/liziyan/MainActivity.java
@@ -0,0 +1,99 @@
+package com.bawei.liziyan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.Toast;
+
+import com.vector.update_app.UpdateAppBean;
+import com.vector.update_app.UpdateAppManager;
+import com.vector.update_app.UpdateCallback;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+        Map<String, String> params = new HashMap<String, String>();
+
+        params.put("appKey", "ab55ce55Ac4bcP408cPb8c1Aaeac179c5f6f");
+        params.put("key1", "value2");
+        params.put("key2", "value3");
+
+        new UpdateAppManager
+                .Builder()
+                //必须设置，当前Activity
+                .setActivity(this)
+                //以下设置，都是可选
+                //设置请求方式，默认get
+                .setPost(false)
+                //添加自定义参数，默认version=1.0.0（app的versionName）；apkKey=唯一表示（在AndroidManifest.xml配置）
+                .setParams(params)
+                //设置点击升级后，消失对话框，默认点击升级后，对话框显示下载进度
+                .hideDialogOnDownloading()
+                //设置头部，不设置显示默认的图片，设置图片后自动识别主色调，然后为按钮，进度条设置颜色
+                .setTopPic(R.mipmap.lib_update_app_top_bg)
+                //为按钮，进度条设置颜色，默认从顶部图片自动识别。
+                //.setThemeColor(ColorUtil.getRandomColor())
+                //设置apk下砸路径，默认是在下载到sd卡下/Download/1.0.0/test.apk
+                .setTargetPath(path)
+                //设置appKey，默认从AndroidManifest.xml获取，如果，使用自定义参数，则此项无效
+                //.setAppKey("ab55ce55Ac4bcP408cPb8c1Aaeac179c5f6f")
+                //不显示通知栏进度条
+                .dismissNotificationProgress()
+                //是否忽略版本
+                //.showIgnoreVersion()
+
+                .build()
+                //检测是否有新版本
+                .checkNewApp(new UpdateCallback() {
+                    /**
+                     * 解析json,自定义协议
+                     *
+                     * @param json 服务器返回的json
+                     * @return UpdateAppBean
+                     */
+                    @Override
+                    protected UpdateAppBean parseJson(String json) {
+                        UpdateAppBean updateAppBean = new UpdateAppBean();
+                        try {
+                            JSONObject jsonObject = new JSONObject(json);
+                            updateAppBean
+                                    //（必须）是否更新Yes,No
+                                    .setUpdate(jsonObject.optString("update"))
+                                    //（必须）新版本号，
+                                    .setNewVersion(jsonObject.optString("new_version"))
+                                    //（必须）下载地址
+                                    .setApkFileUrl(jsonObject.optString("apk_file_url"))
+                                    //（必须）更新内容
+                                    .setUpdateLog(jsonObject.optString("update_log"))
+                                    //大小，不设置不显示大小，可以不设置
+                                    .setTargetSize(jsonObject.optString("target_size"))
+                                    //是否强制更新，可以不设置
+                                    .setConstraint(false)
+                                    //设置md5，可以不设置
+                                    .setNewMd5(jsonObject.optString("new_md51"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        return updateAppBean;
+                    }
+                    /**
+                     * 没有新版本
+                     */
+                    public void noNewApp() {
+                        Toast.makeText(MainActivity.this, "没有新版本", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
